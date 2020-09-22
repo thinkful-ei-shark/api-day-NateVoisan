783018a93202eb7763746ec35f5f3b62a43abbb8
Nate Voisan <rufio96@gmail.com>
2020-09-22 12:41:32 -0700

    all done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import $ from 'jquery';
+  import $ from 'jquery';
 
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
+import store from './store';
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
